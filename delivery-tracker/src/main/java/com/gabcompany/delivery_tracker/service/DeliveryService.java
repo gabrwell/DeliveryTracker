07b5e59a153818fb7bec64d475033cf53ad8e190
gabrwell <gabrielgibin0504@gmail.com>
2026-06-09 23:05:33 -0300

    deliveryService advancing

--- delivery-tracker/src/main/java/com/gabcompany/delivery_tracker/service/DeliveryService.java
+++ delivery-tracker/src/main/java/com/gabcompany/delivery_tracker/service/DeliveryService.java
@@ -1,8 +1,28 @@
 package com.gabcompany.delivery_tracker.service;
 
+import com.gabcompany.delivery_tracker.model.Delivery;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class DeliveryService {
+
+    private List<Delivery> deliveries = new ArrayList<>();
+
+    public Delivery createDelivery(String recipient){
+        String trackingCode = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+
+        Delivery newDelivery = new Delivery(trackingCode, recipient, "CREATED");
+        deliveries.add(newDelivery);
+
+        return newDelivery;
+    }
+
+    public List<Delivery> getAllDeliveries() {
+        return deliveries;
+    }
 
 }
