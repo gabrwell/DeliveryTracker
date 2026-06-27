b637b01dc3eb42cdd750963fdc487b25a2d9a309
gabrwell <gabrielgibin0504@gmail.com>
2026-06-26 22:54:20 -0300

    validations pt1

--- delivery-tracker/src/main/java/com/gabcompany/delivery_tracker/dto/DeliveryRequestDto.java
+++ delivery-tracker/src/main/java/com/gabcompany/delivery_tracker/dto/DeliveryRequestDto.java
@@ -1,6 +1,13 @@
 package com.gabcompany.delivery_tracker.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class DeliveryRequestDto {
+
+    @NotBlank(message = "Recipient name is required and cannot be blank.")
+
+    @Size(min = 3, message = "Recipient name must be at least 3 characters long.")
 
     private String recipient;
 
