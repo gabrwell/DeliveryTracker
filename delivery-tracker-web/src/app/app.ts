0ee1ef574908b5bab9b9685352e45dbf9161d8b7
gabrwell <gabrielgibin0504@gmail.com>
2026-07-16 23:29:54 -0300

    ANGULAR

---
+++ delivery-tracker-web/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+// 1. Importando as peças visuais do Material Design
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('delivery-tracker-web');
+}
