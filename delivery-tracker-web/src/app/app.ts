4ef6a6665d35309438e6acc4c923a865e07e4c2a
gabriel <gabrielgibin0504@gmail.com>
2026-08-12 10:36:42 -0300

    test web

--- delivery-tracker-web/src/app/app.ts
+++ delivery-tracker-web/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-  import { MatCardModule } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
+
+import { DeliveryService } from './services/delivery.services'; 
 
 @Component({
   selector: 'app-root',
@@ -24,13 +25,27 @@ import { FormsModule } from '@angular/forms';
 })
 export class App {
   protected readonly title = signal('delivery-tracker-web');
-
+  
   trackingCode = '';
 
+  deliveryResult: any = null; 
 
-searchDelivery() {
-    alert('The button is working! You searched for: ' + this.trackingCode);
-    console.log('Code captured in TypeScript:', this.trackingCode);
+  constructor(private deliveryService: DeliveryService) {}
+
+  searchDelivery() {
+    if (!this.trackingCode) {
+      return; 
+    }
+
+    this.deliveryService.getDeliveryByCode(this.trackingCode).subscribe({
+      next: (dadosDaApi: any) => {
+        this.deliveryResult = dadosDaApi;
+        console.log('Sucesso! Dados vindos do Java:', this.deliveryResult);
+      },
+      error: (erro: any) => {
+        console.error('Erro na requisição:', erro);
+        this.deliveryResult = null;
+      }
+    });
   }
 }
-
